1e0c51fbaddd30f9cbef70e31db9ade62d833b03
tran ngoc tin <ngoctint1lvc@gmail.com>
2019-03-29 21:51:00 +0700

    nothing

--- config/mongokey.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb://ngoctin:iamtranngoctin@hackathon-shard-00-00-l01pt.mongodb.net:27017,hackathon-shard-00-01-l01pt.mongodb.net:27017,hackathon-shard-00-02-l01pt.mongodb.net:27017/csquiz?ssl=true&replicaSet=hackathon-shard-0&authSource=admin&retryWrites=true'
-}
